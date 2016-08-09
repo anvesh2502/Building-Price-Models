61991bbf1e9630fcf75f8ed11a93ae7252748c86
anvesh2502 <anveshrocks@yahoo.co.in>
2016-08-08 19:50:29 -0500

    Adding K nearest neighbors

--- numpredict.py
+++ numpredict.py
@@ -18,4 +18,52 @@ def wineprice(rating,age) :
 
     if price<0 : return 0
     return price
-            
+
+
+def wineset1() :
+
+    rows=[]
+    for i in range(300) :
+        # Create a random age and rating
+        rating=random()*50+50
+        age=random()*50
+
+        # Get reference price
+        price=wineprice(rating,age)
+
+        # Add some noise
+        price*=(random()*0.4+0.8)
+
+        # Add to the dataset
+        rows.append({'input' : (rating,age),'result' : price})
+
+    return rows
+
+
+def euclidean(v1,v2) :
+    d=0.0
+    for i in range(len(v1)) :
+        d+=(v1[i]-v2[i])**2
+    return math.sqrt(d)
+
+
+def getdistances(data,vec1) :
+
+    distancelist=[]
+    for i in range(len(data)) :
+        vec2=data[i]['input']
+        distancelist.append((euclidean(vec1,vec2),i))
+    distancelist.sort()
+    return distancelist
+
+def knnestimate(data,vec1,k=3) :
+    # Get sorted distances
+    dlist=getdistances(data,vec1)
+    avg=0.0
+
+    # Take the average of the top k results
+    for i in range(k) :
+        idx=dlist[i][1]
+        avg+=data[idx]['result']
+    avg=avg/k
+    return avg
