87cceac5817ecf71654bf120f0be6779a0cb4cef
anvesh2502 <anveshrocks@yahoo.co.in>
2016-08-09 17:45:31 -0500

    Adding scaling to the inputs

--- numpredict.py
+++ numpredict.py
@@ -128,4 +128,24 @@ def crossvalidate(algf,data,trials=100,test=0.05) :
         trainset,testset=dividedata(data,test)
         error+=testalgorithm(algf,trainset,testset)
     return error/trials
-                                
+
+def wineset2() :
+    rows=[]
+    for i in range(300) :
+        rating=random()*50+50
+        age=random()*50
+        aisle=float(randint(1,20))
+        bottlesize=[375.0,750.0,1500.0,3000.0][randint(0,3)]
+        price=wineprice(rating,age)
+        price*=(bottlesize/750)
+        price*=(random()*0.9+0.2)
+        rows.append({'input' : (rating,age,aisle,bottlesize),'result' : price})
+    return rows
+
+def rescale(data,scale) :
+    scaleddata=[]
+    for row in data :
+        scaled=[scale[i]*row['input'][i] for i in range(len(scale))]
+        scaleddata.append({'input' : scaled,'result' : row['result']})
+    return scaleddata
+                
