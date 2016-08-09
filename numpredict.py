e410cf1bfe737d179c3aad753db4e34038925deb
anvesh2502 <anveshrocks@yahoo.co.in>
2016-08-08 20:44:58 -0500

    Added cross validation to the data (0.05 split

--- numpredict.py
+++ numpredict.py
@@ -100,3 +100,32 @@ def weightedknn(data,vec1,k=5,weightf=gaussian) :
         totalweight+=weight
     avg=avg/totalweight
     return avg
+
+
+def dividedata(data,test=0.05) :
+    trainset=[]
+    testset=[]
+
+    for row in data :
+        if random()<test :
+            testset.append(row)
+        else :
+            trainset.append(row)
+    return trainset,testset
+
+
+def testalgorithm(algf,trainset,testset) :
+    error=0.0
+    for row in testset :
+        guess=algf(trainset,row['input'])
+        error+=(row['result']-guess)**2
+    return error/len(testset)
+
+
+def crossvalidate(algf,data,trials=100,test=0.05) :
+    error=0.0
+    for i in range(trials) :
+        trainset,testset=dividedata(data,test)
+        error+=testalgorithm(algf,trainset,testset)
+    return error/trials
+                                
